e8032f59fb89648ad478dd9e1565a19d0cfb278a
default <trisha.ray@stu.ocsb.ca>
2025-03-21 16:51:51 +0000

    answer php

--- answer.php
+++ answer.php
@@ -1,60 +1,66 @@
 <!DOCTYPE html>
 <!-- ICS2O-Unit2-04-HTML-MDL -->
 <html lang="en-ca">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="description" content="The XXX program, PHP" />
-    <meta name="keywords" content="mths, icd2o" />
-    <meta name="author" content="Mr. Coxall" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    />
-    <link
-      rel="stylesheet"
-      href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css"
-    />
-    <link rel="stylesheet" href="./css/style.css" />
-        <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="./apple-touch-icon.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="./favicon-32x32.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="./favicon-16x16.png"
-    />
-    <link rel="manifest" href="./site.webmanifest" />
-    <title>The XXX program, PHP</title>
-  </head>
 
-  <body>
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">The XXX program, PHP</span>
-        </div>
-      </header>
-      <main class="mdl-layout__content">
-        <div class="right-image">
-          <img src="./images/image.png" alt="computer image">
-        </div>
-        <div class="page-content">The answer is ...</div>
-        <br />
-        <div class="page-content-answer">
-          <a href="./index.php">Return ...</a>
-        </div>
-      </main>
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Area of Triangle, PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Trisha Ray" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="./apple-touch-icon.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="./favicon-32x32.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="./favicon-16x16.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>Area of Triangle, PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Area Of Triangle, PHP</span>
+      </div>
+  </div>
+  <div class="page-content-php">
+    <div id="area-triangle">
+      <?php
+      $lengthOfRectangle = $_GET["base-length"];
+      $widthOfRectangle = $_GET["height-length"];
+      
+      // process
+      $area = $baselength * $heigthlength;
+      // output
+      echo "If a Triangle has a base length of " . $baselength . " cm and a height of = " . $widthOfRectangle . " cm:";
+      echo "<br />";
+      echo "<br />";
+      echo "The area of the Triangle is " . $area . " cm².";
+      echo "<br />";
+      ?>
     </div>
-  </body>
+    <div class="page-content-answer">
+      <a href="./index.php">Return...</a>
+  </div>
+  </main>
+  </div>
+</body>
 </html>
